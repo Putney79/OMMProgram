40eb52ecd3ac69cbbbde197385ed47327dd73816
Jarrell Witherspoon <79674325+jarrell-w@users.noreply.github.com>
2023-10-30 16:32:23 -0400

    add test login page

--- loginbg.js
+++ loginbg.js
@@ -9,3 +9,21 @@ window.onload = function() {
   document.body.style.backgroundSize = "cover";
   document.body.style.backgroundRepeat = "no-repeat";
 };
+
+let user1 = {
+  username: 'admin',
+  password: 'password'
+}
+
+const verifyLogin = (event) => {
+  event.preventDefault()
+  const username = document.getElementById('username').value
+  const password = document.getElementById('password').value
+  if(username === user1.username && password === user1.password) {
+    window.location.href = "/admin.html"
+  } else {
+    alert("incorrect details.")
+  }
+}
+
+document.getElementById('submit-login').addEventListener('click', verifyLogin)
