3f658b7a84255fd99399876f737d01c7bb5c236a
Nickolas Putney <148489947+Putney79@users.noreply.github.com>
2023-11-02 16:26:01 -0400

    Update createtest.js

--- createtest.js
+++ createtest.js
@@ -15,28 +15,3 @@ window.onload = function(){
     });
   });
 }
-
-var categoryCheckbox = document.querySelector('#categoryCheckbox');
-var bodyRegionCheckbox = document.querySelector('#bodyRegionCheckbox');
-var treatmentTechniquesCheckbox = document.querySelector('#treatmentTechniquesCheckbox');
-var categoryNestedCheckbox = document.querySelector('#nestedCategoryCheckbox');
-var bodyRegionNestedCheckbox = document.querySelector('#nestedBodyRegionCheckbox');
-var treatmentTechniquesNestedCheckbox = document.querySelector('#nestedTreatmentTechniquesCheckbox');
-// Enable the Generate Test button if the appropriate conditions are met
-function enableGenerateTestButton() {
-  if (
-    (categoryCheckbox.checked || Array.from(categoryNestedCheckbox.querySelectorAll('input[type="checkbox"]')).some(cb => cb.checked)) ||
-    (bodyRegionCheckbox.checked || Array.from(bodyRegionNestedCheckbox.querySelectorAll('input[type="checkbox"]')).some(cb => cb.checked)) ||
-    (treatmentTechniquesCheckbox.checked || Array.from(treatmentTechniquesNestedCheckbox.querySelectorAll('input[type="checkbox"]')).some(cb => cb.checked))
-  ) {
-    generateTestBtn.disabled = false;
-  } else
-    generateTestBtn.disabled = true;
-}
-// Check if generateTestBtn should be enabled whenever a checkbox is clicked
-categoryCheckbox.addEventListener('change', enableGenerateTestButton);
-categoryNestedCheckbox.addEventListener('change', enableGenerateTestButton);
-bodyRegionCheckbox.addEventListener('change', enableGenerateTestButton);
-bodyRegionNestedCheckbox.addEventListener('change', enableGenerateTestButton);
-treatmentTechniquesCheckbox.addEventListener('change', enableGenerateTestButton);
-treatmentTechniquesNestedCheckbox.addEventListener('change', enableGenerateTestButton);
