00066ade92233feceb7b66a9bc05f95b88feef83
Nickolas Putney <148489947+Putney79@users.noreply.github.com>
2023-11-02 16:03:19 -0400

    Update createtest.js

--- createtest.js
+++ createtest.js
@@ -16,18 +16,43 @@ window.onload = function(){
   });
 }
 
-var categoryCheckbox = document.getElementById('categoryCheckboxId');
-var bodyRegionCheckbox = document.getElementById('bodyRegionCheckboxId');
-var treatmentTechniqueCheckbox = document.getElementById('treatmentTechniqueCheckboxId');
-var generateTestBtn = document.getElementById('generateTestBtn');
-var checkboxes = [categoryCheckbox, bodyRegionCheckbox, treatmentTechniqueCheckbox];
-checkboxes.forEach(function(checkbox){
-  checkbox.addEventListener('change', function(){
-    if(categoryCheckbox.checked || bodyRegionCheckbox.checked || treatmentTechniqueCheckbox.checked){
-      generateTestBtn.disabled = false;
+const checkboxes = ["categoryCheckbox", "bodyRegionCheckbox", "treatmentTechniquesCheckbox"];
+const nestedCheckboxes = ["nestedCategoryCheckbox", "nestedBodyRegionCheckbox", "nestedTreatmentTechniquesCheckbox"];
+let btnGenerateTest;
+window.onload = function() {
+    btnGenerateTest = document.createElement("button");
+    btnGenerateTest.innerHTML = "Generate Test";
+    btnGenerateTest.disabled = true;
+    btnGenerateTest.style.position = "fixed";
+    btnGenerateTest.style.bottom = "5px";
+    btnGenerateTest.style.left = "5px";
+    document.body.appendChild(btnGenerateTest);
+    checkboxes.forEach((id, index) => {
+        const chkBox = document.getElementById(id);
+        if (chkBox != null) {
+            chkBox.addEventListener("change", function() {
+                updateButtons(this, nestedCheckboxes[index]);
+            });
+        }
+    });
+    nestedCheckboxes.forEach((id) => {
+        const nestedCheckboxes = document.getElementById(id);
+        const checkboxes = nestedCheckboxes.getElementsByTagName("input");
+        for (let i = 0; i < checkboxes.length; i++) {
+            checkboxes[i].addEventListener("change", function() {
+                updateButtons(this, id);
+            });
+        }
+    });
+};
+function updateButtons(checkbox, nestedId) {
+    const nestedCheckboxes = document.getElementById(nestedId);
+    const checkboxes = nestedCheckboxes.getElementsByTagName("input");
+    let atLeastOneChecked = checkbox.checked;
+    for (let i = 0; i < checkboxes.length && !atLeastOneChecked; i++) {
+        if (checkboxes[i].checked) {
+            atLeastOneChecked = true;
+        }
     }
-    else{
-      generateTestBtn.disabled = true;
-    }
-  });
-});
+    btnGenerateTest.disabled = !atLeastOneChecked;
+}
